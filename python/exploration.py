06454814b2eb803c6104d41b4022b963446d108c
Chad Baker <chad.baker@nrel.gov>
2023-02-10 09:09:03 -0700

    plots current results

---
+++ python/exploration.py
@@ -0,0 +1,32 @@
+# %%
+import numpy as np
+import json
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
+# %%
+with open("../temp_results.json", 'r') as file:
+    results = json.load(file)
+
+# %%
+fig, ax = plt.subplots()
+ax.plot(
+    results['history']['time'],
+    results['m1']['history']['temp'][1:],
+    label='m1',
+)
+ax.plot(
+    results['history']['time'],
+    results['m2']['history']['temp'][1:],
+    label='m2',
+)
+ax.plot(
+    results['history']['time'],
+    results['m3']['history']['temp'][1:],
+    label='m3',
+)
+ax.set_ylabel('Temperature [°C]')
+ax.set_xlabel('Time [s]')
+ax.legend()
+# %%
