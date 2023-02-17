901acc4186c5a8fee0e88e357b845fb3844e7262
Chad Baker <chad.baker@nrel.gov>
2023-02-17 11:25:09 -0700

    more progress toward pyo3 api

---
+++ dss-core/proc-macros/src/pyo3_api.rs
@@ -0,0 +1,6 @@
+use crate::imports::*;
+
+/// Derives several methods for struct
+pub(crate) fn pyo3_api(_attr: TokenStream, input: TokenStream) -> TokenStream {
+    input
+}
--- dss-core/src/imports.rs
+++ dss-core/src/imports.rs
@@ -1,3 +1,6 @@
-pub(crate) use serde::{Deserialize, Serialize};
+#[cfg(feature = "pyo3")]
+pub(crate) use crate::pyo3::*;
+
 pub(crate) use anyhow::anyhow;
 pub(crate) use proc_macros::*;
+pub(crate) use serde::{Deserialize, Serialize};
