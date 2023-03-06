d3ff0212496c005a0adf9c4924f18eefcb9621c0
Chad Baker <chad.baker@nrel.gov>
2023-03-06 14:05:14 -0700

    checks that python scripts can run

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,3 +1,10 @@
 (cd dess-examples/ && cargo test && \
 cd ../dess-pyo3/ && maturin develop --release) \
 && python -m unittest
+
+(
+    cd python/Sytem3TM/ && 
+    for file in *.py; do 
+        python "$file"
+    done
+)
