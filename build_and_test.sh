0bfe5e66061292bed2054c973c1083349b886e35
Chad Baker <chad.baker@nrel.gov>
2023-03-16 13:53:14 -0600

    better folder name

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,4 +1,4 @@
 (cd dess-examples/ && cargo test && \
-cd ../dess-pyo3/ && maturin develop --release) \
+cd ../dess-examples-pyo3/ && maturin develop --release) \
 && python -m unittest \
 && sh run_py_scripts.sh
