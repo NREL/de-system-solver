e1e9a26b4f360e5efccf6959440377a146541f82
Chad Baker <chad.baker@nrel.gov>
2023-05-16 16:45:26 -0600

    better functional test coverage

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,4 +1,4 @@
 (cd dess-examples/ && cargo test && \
-cd ../dess-examples-pyo3/ && maturin develop --release) \
-&& python -m unittest \
-&& sh run_py_scripts.sh
+cd ../dess-examples-pyo3/ && maturin develop --release) && \
+# pytest -v tests && \ # make a tests/ folder
+sh run_py_scripts.sh
--- run_py_scripts.sh
+++ run_py_scripts.sh
@@ -3,4 +3,10 @@
     for file in *.py; do 
         python "$file"
     done
+) && \
+(
+    cd python/System3TMWithBC/ && 
+    for file in *.py; do 
+        python "$file"
+    done
 )
